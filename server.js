22819e16b0f8e127ef11578a1f5a65cf0515e10a
Mike Rhiney <rhineyman@gmail.com>
2021-04-28 20:19:45 -0700

    server infinte ping

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const session = require('express-session');
-const routes = require('./controllers');
-const exphbs = require('express-handlebars');
+// const routes = require('./controllers');
+// const exphbs = require('express-handlebars');
 const path = require('path');
 
 
@@ -9,7 +9,6 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.nextTick.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
